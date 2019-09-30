150f8dbdd82d24c30d4180952240debce49f9a19
Brijesk <brijeshk_raj@yahoo.com>
2019-09-30 11:04:56 +0530

    Initial Commit

---
+++ src/main/java/com/philips/App.java
@@ -0,0 +1,18 @@
+package com.philips;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.philips.customer.bo.CustomerBo;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("Spring-Customer.xml");
+
+		CustomerBo customer = (CustomerBo) appContext.getBean("customerBo");
+		
+		customer.addCustomerAround("philips_user1");
+
+	}
+}
---
+++ src/main/java/com/philips/customer/bo/CustomerBo.java
@@ -0,0 +1,12 @@
+package com.philips.customer.bo;
+
+public interface CustomerBo {
+
+	void addCustomer();
+	
+	String addCustomerReturnValue();
+	
+	void addCustomerThrowException() throws Exception;
+	
+	void addCustomerAround(String name);
+}
